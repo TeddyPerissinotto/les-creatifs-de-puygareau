4bd8520d33e612764bb4c488d55b7e6d09459a5c
teddy_perissinotto <teddy.perissinotto@lapiscine.pro>
2019-08-31 10:05:26 +0200

    30/08/2019

---
+++ src/Controller/AdminMediaController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Media;
+use App\Form\MediaType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminMediaController extends AbstractController
+{
+    /**
+     * @Route("/admin/media/insert", name="media_form_insert")
+     */
+    public function mediaFormInsert(Request $request, EntityManagerInterface $entityManager)
+    {
+        $media = new Media();
+
+        $form = $this->createForm(MediaType::class, $media);
+        $mediaFormView = $form->createView();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($media);
+            $entityManager->flush();
+        }
+        return $this->render('admin/adminFormMedia.html.twig',
+            [
+                'mediaFormView' => $mediaFormView
+            ]);
+
+    }
+
+}
